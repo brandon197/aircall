87feee2518b47734d5d67eea4e45892c521d8a3d
brandon197 <brandonchan.bnc@gmail.com>
2022-06-23 20:34:28 -0400

    archive and unarchive working

--- src/CallItem.jsx
+++ src/CallItem.jsx
@@ -1,35 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
 import { Badge } from "@mui/material";
 
 import { DateTime } from "luxon";
 
-// import ImageIcon from '@mui/icons-material/Image';
-// import WorkIcon from '@mui/icons-material/Work';
-// import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import axios from "axios";
+import { async } from "regenerator-runtime";
 
-const CallItem = (props) => {
-    const [isArchived, setIsArchived] = useState(props.obj.is_archived);
-    
-
+const CallItem = ({ obj, dialogSetter, fetchCalls }) => {
+  const handleArchive = async (toggle) => {
+    try {
+      const res = await axios.post(
+        `https://aircall-job.herokuapp.com/activities/${obj.id}`,
+        { is_archived: toggle }
+      );
+      fetchCalls();
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-      <ListItemButton style={{display:'flex'}}>
+    <div style={{ display: "flex" }}>
+      <ListItemButton onClick={() => dialogSetter(true)}>
         <ListItemAvatar>
           <Avatar>{/* <ImageIcon /> */}</Avatar>
         </ListItemAvatar>
         <Badge badgeContent={4} color="secondary">
           <ListItemText
-            primary={props.obj.from}
-            secondary={"tried to call " + props.obj.to}
+            primary={obj.from}
+            secondary={"tried to call " + obj.to}
           />
         </Badge>
-        <div style={{justifySelf:'end'}}>{DateTime.fromISO(props.obj.created_at).toFormat("t")}</div>
+        <div style={{ justifySelf: "end" }}>
+          {DateTime.fromISO(obj.created_at).toFormat("t")}
+        </div>
       </ListItemButton>
+      <Button onClick={() => handleArchive(!obj.is_archived)}>
+        {obj.is_archived ? "undo-Archive" : "Archive"}
+      </Button>
+    </div>
   );
 };
 
--- src/CallList.jsx
+++ src/CallList.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
+
 import List from "@mui/material/List";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+
 import "regenerator-runtime";
 
 import { Button, ButtonGroup } from "@mui/material";
@@ -11,22 +15,39 @@ import CallItem from "./CallItem.jsx";
 const CallList = () => {
   const [list, setList] = useState([]);
   const [showArchived, setShowArchived] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  const handleClose = () => {
+    setShowDialog(false);
+  };
+
+  async function fetchCalls() {
+    setLoading(true);
+    const res = await axios.get("https://aircall-job.herokuapp.com/activities");
+    console.log(res.data);
+
+    setList(res.data.filter((e) => e.is_archived == showArchived));
+    setLoading(false);
+  }
+  async function fetchCall(id) {
+    const res = await axios.get(
+      `https://aircall-job.herokuapp.com/activities/${id}`
+    );
+    console.log(res.data);
+  }
   useEffect(() => {
-    async function fetchCalls() {
-      const res = await axios.get(
-        "https://aircall-job.herokuapp.com/activities"
-      );
-      console.log(res.data);
-
-      setList(res.data.filter((e) => e.is_archived == showArchived));
-    }
     fetchCalls();
   }, [showArchived]);
 
   return (
     <div className="main">
-          <ButtonGroup style={{ display: 'flex', justifyContent:'center'}} variant="text" aria-label="text button group">
+      <ButtonGroup
+        style={{ display: "flex", justifyContent: "center" }}
+        variant="text"
+        aria-label="text button group"
+        sx={{ my: 1 }}
+      >
         <Button color="success" onClick={() => setShowArchived(false)}>
           Call History
         </Button>
@@ -34,11 +55,40 @@ const CallList = () => {
           Archived
         </Button>
       </ButtonGroup>
-      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-        {list.map((item, index) => (
-          <CallItem key={index} obj={item} />
-        ))}
-      </List>
+      {list.length < 1 ? (
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "grey",
+          }}
+        >
+          No items to Display
+        </div>
+      ) : (
+        <div>
+          <Dialog
+            onClose={handleClose}
+            open={showDialog}
+            style={{ width: "100%" }}
+          >
+            <DialogTitle>Title</DialogTitle>
+            <div>Hi</div>
+          </Dialog>
+          <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+            {list.map((item, index) => (
+              <CallItem
+                dialogSetter={(e) => setShowDialog(e)}
+                fetchCalls={() => fetchCalls()}
+                key={index}
+                obj={item}
+              />
+            ))}
+          </List>
+        </div>
+      )}
     </div>
   );
 };
